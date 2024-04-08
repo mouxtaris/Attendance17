63c2cc7ee41a6654b6345b568ba9d14f32bf9f54
mouxtaris <dimouch@cs.ihu.gr>
2024-04-08 13:36:05 +0300

    Align Elements in AddStudent

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.openjfx.attendance {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens org.openjfx.attendance17 to javafx.fxml;
     exports org.openjfx.attendance17;
 }
